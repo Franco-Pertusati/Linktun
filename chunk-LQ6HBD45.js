98bd1650f2bfea61ca0db24090d1dd49929129d5
Franco Pertusati <pertusatifranco836@gmail.com>
2024-11-21 01:32:49 -0300

    First commit

---
+++ chunk-LQ6HBD45.js
@@ -0,0 +1,1 @@
+import{b as o}from"./chunk-GSHX4BJS.js";import"./chunk-UINAX72C.js";import"./chunk-TMVVO5PM.js";import"./chunk-ACKELEN3.js";var m=[{path:"home",component:o}];export{m as default};
---
+++ chunk-ZIF5N4OY.js
@@ -0,0 +1,1 @@
+import"./chunk-ACKELEN3.js";var o=[{path:"sign-in",loadComponent:()=>import("./chunk-KDCATATF.js")},{path:"log-in",loadComponent:()=>import("./chunk-2XHZYBYN.js")}];export{o as default};
